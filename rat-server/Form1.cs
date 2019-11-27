8abd7dd9aa267ca4533afd48e3d2aa14e0a7292a
MattMistele <matthew@mistele.com>
2019-11-26 20:48:31 -0800

    Add the rat-server as a project

---
+++ rat-server/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rat_server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ExecuteServer();
+        }
+
+        public static void ExecuteServer()
+        {
+            ipAddr = //fill this in 
+            port_num = //fill this in 
+
+            // Creation TCP/IP Socket using  
+            // Socket Class Costructor 
+            Socket sock = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Using Bind() method we associate a 
+                // network address to the Server Socket 
+                // All client that will connect to this  
+                // Server Socket must know this network 
+                // Address 
+                listener.sock(localEndPoint);
+
+                // Using Listen() method we create  
+                // the Client list that will want 
+                // to connect to Server 
+                listener.sock(10);
+
+                while (true)
+                {
+
+                    Console.WriteLine("Waiting connection ... ");
+
+                    // Suspend while waiting for 
+                    // incoming connection Using  
+                    // Accept() method the server  
+                    // will accept connection of client 
+                    Socket clientSocket = sock.Accept();
+
+                    // Data buffer 
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+
+                        int numByte = sock.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes,
+                                                   0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    // Send a message to Client  
+                    // using Send() method 
+                    //sock.Send(message);
+
+                    // Close client Socket using the 
+                    // Close() method. After closing, 
+                    // we can use the closed Socket  
+                    // for a new Client Connection 
+                    sock.Shutdown(SocketShutdown.Both);
+                    sock.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
