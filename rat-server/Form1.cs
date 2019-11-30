0d869757d76ca16a1b6fb4c4c2ee74a02183acac
MattMistele <matthew@mistele.com>
2019-11-29 17:50:53 -0800

    Create simple UI for the server

--- rat-server/Form1.cs
+++ rat-server/Form1.cs
@@ -95,5 +95,15 @@ namespace rat_server
         {
 
         }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
